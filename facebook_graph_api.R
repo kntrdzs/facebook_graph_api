a94ec8351cf8f1e6297d0ea6c4b06dafd4675531
Kántor Dezső <kantor.dezso@multicontact.local>
2018-05-03 11:07:49 +0200

    Működő api, volt a régi json struktúrával. Átírás szükséges

--- facebook_graph_api.R
+++ facebook_graph_api.R
@@ -1,4 +1,5 @@
 #### FACEBOOK GRAPH API ####
+#https://graph.facebook.com/v2.8/search?access_token=EAACEdEose0cBACn6ZBlet9dqwfIBWi132MM8YuYHTwZCvAYa4v4S4t7ph1ZAWBtx2ycfZARm8z47ZAKiX0vNgepRCqt2hcZCEGcn4ZBEdYNTFOAZAvLKY5O8a8ZCDy6IiMxeOhkv5GYsHXl1ZAhwRbcn99YfM0KgCVPGmwvevTZCGTAklB1tGAUg7bXmUzUzB36ZAkjcW5aC1C5jOAZDZD&pretty=1&fields=name,single_line_address,phone,location,fan_count,overall_star_rating,price_range,link&q=alapitvany&center=47.49801,19.03991&distance=20000&type=page&limit=300&after=MjYZD
 
 facebook_api <- function(token, keyword, type, limit,center,distance) {
   
@@ -11,8 +12,8 @@ facebook_api <- function(token, keyword, type, limit,center,distance) {
   adat <-try(
     fromJSON(
       paste0(
-        "https://graph.facebook.com/v2.10/search?access_token=",token,
-        "&pretty=1&fields=name%2Csingle_line_address%2Cphone%2Clocation%2Cfan_count&q=",keyword[i],
+        "https://graph.facebook.com/v3.0/search?access_token=",token,
+        "&pretty=1&fields=name,single_line_address,phone,location,fan_count,overall_star_rating,price_range&q=",keyword[i],
         "&center=",center,
         "&distance=",distance,
         "&type=",type,
